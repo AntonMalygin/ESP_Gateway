dcca4a9f8f0db4a8974b703fb151e7e106dc8511
Anton <malygin.anton@mail.ru>
2022-02-06 16:58:48 +0300

    06022022

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "main.h"
 #include "EmbUI.h"
 #include "uistrings.h"   // non-localized text-strings
-
+#include "radio.h"
 
 /**
  * построение интерфейса осуществляется в файлах 'interface.*'
@@ -12,6 +12,10 @@
 
 #define RXD2 16	//(RX2)
 #define TXD2 17	//(TX2)
+
+radio_data1 *rd; // Структура данных для MSG ID = 1 . Простые данные от часов
+
+void rx_radio_filter(radio_frame * msg);
 
 
 
@@ -43,6 +47,7 @@ void loop() {
   embui.handle();
 
 // Тут будем считатывать данные с Юарт и передавать куда нужно. не написано ещё 
+radio_pool();
 
   while (Serial2.available()) 
   {        
@@ -50,5 +55,22 @@ void loop() {
     // По идее бы радио обмен нужно реализовать как отдельную задачу.
   
   }
+
+}
+
+void rx_radio_filter(radio_frame * msg)
+{
+ uint8_t *bf = (uint8_t *)msg;
+if (msg->msgid == 1)
+{
+  //radio_data1 *rd = ( radio_data1 *)msg->data;
+  //rd=msg->data;
+ rd=(radio_data1*)msg->data;
+
+// Отправляем в HC12, то что пришло по UART от часов. 
+Serial2.write(bf,msg->len+7);
+//Serial2.write((uint8_t *)msg,msg->len+7);
+
+}
 
 }
